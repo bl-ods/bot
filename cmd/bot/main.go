87eff2476ccc57d37f9150370f9a0793c146bfe1
Roma <www.roma.ibr@gmail.com>
2023-10-16 19:06:33 +0300

    First iteration

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1"),
+		tgbotapi.NewKeyboardButton("2"),
+		tgbotapi.NewKeyboardButton("3"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4"),
+		tgbotapi.NewKeyboardButton("5"),
+		tgbotapi.NewKeyboardButton("6"),
+	),
+)
+
+const token = "6464001705:AAGCGyq7c6PB084ZbSQpX6xKMN959mxX7K8"
 
 func main() {
-	fmt.Println("alloaw")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You bunny wrote:"+update.Message.Text)
+
+		switch update.Message.Text {
+		case "open":
+			msg.ReplyMarkup = numericKeyboard
+		case "close":
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		}
+		//msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github/Gear19/bot
 
 go 1.21.2
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
